026047ef875c2393699ba4cc2b6dac80272dfdda
Juyoung Jung <jyjung@vuno.co>
2020-02-12 17:49:49 +0900

    Chapter 4: Database

--- flask_tutorial/microblog.py
+++ flask_tutorial/microblog.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+# flask shell 에서 call 할 수 있도록 세팅한다.
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
