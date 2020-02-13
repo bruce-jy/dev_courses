796866ee20f2d793acc3d88d0992acb19eaaa405
Juyoung Jung <jyjung@vuno.co>
2020-02-13 13:46:05 +0900

    Chapter 6: Profile Page and Avatars

--- flask_tutorial/app/models.py
+++ flask_tutorial/app/models.py
@@ -2,14 +2,16 @@ from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')  # Post table에서의 관계를 정의
-                                                                       # backref명으로 조회
+    posts = db.relationship('Post', backref='author', lazy='dynamic')  # Post table에서의 관계를 정의 backref명으로 조회
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     # The __repr__ method tells Python how to print objects of this class, which is going to be useful for debugging
     def __repr__(self):
@@ -20,6 +22,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 @login.user_loader
 def load_user(id):
