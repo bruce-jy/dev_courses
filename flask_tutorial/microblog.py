9ac64491b93021b9cabccfc3e59583d187249766
Juyoung Jung <jyjung@vuno.co>
2020-02-17 14:51:02 +0900

    Chapter 15: A Better Application Structure
    pip install -r requirements.txt로 패키지 설치가능
    전체적인 구조 변경

--- flask_tutorial/microblog.py
+++ flask_tutorial/microblog.py
@@ -1,5 +1,8 @@
-from app import app, db, cli
+from app import create_app, db, cli
 from app.models import User, Post
+
+app = create_app()
+cli.register(app)
 
 # flask shell 에서 call 할 수 있도록 세팅한다.
 @app.shell_context_processor
